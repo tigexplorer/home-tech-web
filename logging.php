8a415efd0e2eab2ec64382739df4a07a46aad524
Joerg Sorge <joergsorge@gmail.com>
2017-01-10 17:14:10 +0100

    added basic files

---
+++ logging.php
@@ -0,0 +1,44 @@
+<?php
+/** 
+* edit notes
+*
+* PHP version 5
+*
+* @category Site
+* @package  Notes
+* @author   Joerg Sorge <joergsorge@googel.com>
+* @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+* @link     http://www.joergsorge.de
+*/
+
+require "../../scripts_home-tech/lib_db.inc.php";
+require "../../scripts_home-tech/lib_common.inc.php";
+		
+// check action	
+if ( isset($_GET['action']) ) {	
+    $action = $_GET['action'];		
+    $action_ok = "yes";
+}
+# TODO else pa und pb
+if ( $action_ok == "yes" ) {		
+    if ( isset($_GET['pa']) ) {	
+        $field = $_GET['pa'];
+    }
+    if ( isset($_GET['pb']) ) {	
+        $value1 = $_GET['pb'];
+    }
+
+} else {
+	$message = "No Command. Nothing to do...";
+	return $message;
+}
+// switch action 
+switch ( $action ):
+	case "add":    
+		$new_id = db_query_add_item("ht_temp", $field, $value1);
+		$message = $new_id;
+		break;
+
+endswitch;
+echo $message;
+?>
