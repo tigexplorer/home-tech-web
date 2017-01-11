39cb9932ee51e4c0ed2e132301f8a2dbd001a81a
Joerg Sorge <joergsorge@gmail.com>
2017-01-11 15:06:07 +0100

    added basic ajax

---
+++ parts/ajax/load_temp.php
@@ -0,0 +1,23 @@
+<?php
+/** 
+* view temp
+*
+* PHP version 5
+*
+* @category Ajax
+* @package  Home-Tech
+* @author   Joerg Sorge <joergsorge@googel.com>
+* @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+* @link     http://www.joergsorge.de
+*/
+
+require "../../scripts_home-tech/lib_db.inc.php";
+
+if ( isset($_GET['pa']) ) {	
+		$sensor_nr = $_GET['pa'];
+}
+	
+$condition = "sensor_nr=".$sensor_nr." ORDER BY id DESC LIMIT 1";   
+$tbl_row = db_query_display_item_1("ht_temp", $condition);
+echo $tbl_row["temp"] / 1000;
+?>
