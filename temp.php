9fcd72e7d523988f869f8334b0d499da812c88d1
Joerg Sorge <joergsorge@gmail.com>
2017-01-11 13:34:25 +0100

    fixed typo

--- temp.php
+++ temp.php
@@ -67,7 +67,7 @@ endswitch;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Home-Tech</title>
-	<?php echo $message;?>
+	<?php echo $temp;?>
     <!-- Bootstrap -->
     <link href="parts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
