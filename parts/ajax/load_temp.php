261ca9a3af993fd50dc299ef74574e9e3887a119
Joerg Sorge <joergsorge@gmail.com>
2017-01-12 20:43:05 +0100

    fiddle timer

--- parts/ajax/load_temp.php
+++ parts/ajax/load_temp.php
@@ -35,7 +35,7 @@ $tbl_rows = db_query_list_items_1( "ht_temp", "temp", $condition );
 $condition = "sensor_nr=".$sensor_nr." ORDER BY id DESC LIMIT 1";   
 $tbl_row = db_query_display_item_1("ht_temp", $condition);
 $temp = substr( $tbl_row["temp"] / 1000, 0, 2);
-$tem_old = substr( $tbl_rows[1]["temp"] / 1000, 0, 2);
+$temp_old = substr( $tbl_rows[1]["temp"] / 1000, 0, 2);
 // select arrow
 if ( $tbl_rows[1]["temp"] > $tbl_rows[0]["temp"] ){
 	$arrow = ' <span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span>';		
